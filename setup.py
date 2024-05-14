49d41eff6147bb804a57ed922d9c8bef77479e67
YaoYinYing <33014714+YaoYinYing@users.noreply.github.com>
2024-05-14 16:13:04 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,9 +7,11 @@ from setuptools.command.build_ext import build_ext
 # Define the target include directory
 python_include_path = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(sys.executable)), '..', 'include'))
 
+package_dir=os.path.dirname(os.path.abspath(__file__))
+
 def copy_headers():
-    print(f'Copying headers nvtx/c/include/nvtx3 to {python_include_path}')
-    shutil.copytree('nvtx/c/include/nvtx3',os.path.join(python_include_path,'nvtx3'))
+    print(f'Copying headers {package_dir}/nvtx/c/include/nvtx3 to {python_include_path}')
+    shutil.copytree(os.path.join(package_dir, 'nvtx/c/include/nvtx3'),os.path.join(python_include_path,'nvtx3'))
 
 class CustomBuildExt(build_ext):
     def run(self):
